c9d30cd35aaf751a6861ce63f87500a82bc0299f
Mike Schoonover <mike@skoonie.com>
2015-09-28 18:24:30 -0500

    Modified filters.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

---
+++ filters/Butterworth 1-3MHz to 3-2MHz 6th order.c
@@ -0,0 +1,94 @@
+/**************************************************************
+WinFilter version 0.8
+http://www.winfilter.20m.com
+akundert@hotmail.com
+
+Filter type: Band Pass
+Filter model: Butterworth
+Filter order: 6
+Sampling Frequency: 66 MHz
+Fc1 and Fc2 Frequencies: 1.300000 MHz and 3.200000 MHz
+Coefficents Quantization: 16-bit
+
+Z domain Zeros
+z = -1.000000 + j 0.000000
+z = -1.000000 + j 0.000000
+z = -1.000000 + j 0.000000
+z = -1.000000 + j 0.000000
+z = -1.000000 + j 0.000000
+z = -1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+z = 1.000000 + j 0.000000
+
+Z domain Poles
+z = 0.888674 + j -0.145398
+z = 0.888674 + j 0.145398
+z = 0.903366 + j -0.194126
+z = 0.903366 + j 0.194126
+z = 0.953553 + j -0.096657
+z = 0.953553 + j 0.096657
+z = 0.887453 + j -0.231252
+z = 0.887453 + j 0.231252
+z = 0.987017 + j -0.135405
+z = 0.987017 + j 0.135405
+z = 0.924121 + j -0.285213
+z = 0.924121 + j 0.285213
+***************************************************************/
+#define Ntap 31
+
+#define DCgain 262144
+
+__int16 fir(__int16 NewSample) {
+    __int16 FIRCoef[Ntap] = { 
+        -12590,
+        -13634,
+        -14019,
+        -13633,
+        -12407,
+        -10327,
+        -7446,
+        -3879,
+          198,
+         4555,
+         8927,
+        13026,
+        16571,
+        19310,
+        21042,
+        21642,
+        21042,
+        19310,
+        16571,
+        13026,
+         8927,
+         4555,
+          198,
+        -3879,
+        -7446,
+        -10327,
+        -12407,
+        -13633,
+        -14019,
+        -13634,
+        -12590
+    };
+
+    static __int16 x[Ntap]; //input samples
+    __int32 y=0;            //output sample
+    int n;
+
+    //shift the old samples
+    for(n=Ntap-1; n>0; n--)
+       x[n] = x[n-1];
+
+    //Calculate the new output
+    x[0] = NewSample;
+    for(n=0; n<Ntap; n++)
+        y += FIRCoef[n] * x[n];
+    
+    return y / DCgain;
+}
