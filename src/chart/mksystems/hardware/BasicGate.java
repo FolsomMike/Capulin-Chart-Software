af46725a3af0c56db32041cdb988d97b4f8ab64c
Mike Schoonover <mike@skoonie.com>
2016-08-17 15:17:54 -0500

    Fixed bug where DAC gain was not being applied at ends of gate.
    Fixed by adjusting calculation of gate width. Using start-end results in
    a width one pixel short. Need to add one to get the true width.
    
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- src/chart/mksystems/hardware/BasicGate.java
+++ src/chart/mksystems/hardware/BasicGate.java
@@ -208,7 +208,7 @@ public void calculateGateTimeLocation(double pUSPerPixel, int pDelayPix,
     double nGateStart =  (gatePixStart + pDelayPix) * pUSPerPixel;
     gateStart.setValue(nGateStart, false);
 
-    double nGateWidth = (gatePixEnd - gatePixStart) * pUSPerPixel;
+    double nGateWidth = (gatePixEnd - gatePixStart + 1) * pUSPerPixel;
     gateWidth.setValue(nGateWidth, false);
 
     int nPixLevel = gatePixLevel; //use a temp variable - don't modify original
